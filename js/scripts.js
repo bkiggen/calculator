4eac0740bcfd6e0259b28114994e7f61fe449dbe
Ben Kiggen and William Wittenbrock <ben@>
2018-08-08 09:31:52 -0700

    Add 1st and 2nd forms and submit button in html.

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,16 @@
 
 
 var add = function(number1, number2) {
-  return parseInt(number1) + parseInt(number2);
+  return number1 + number2;
 };
 var subtract = function(number1, number2) {
-  return parseInt(number1) - parseInt(number2);
+  return number1 - number2;
 };
 var divide = function(number1, number2) {
-  return parseInt(number1) / parseInt(number2);
+  return number1 / number2;
 };
 var multiply = function(number1, number2) {
-  return parseInt(number1) * parseInt(number2);
+  return number1 * number2;
 };
 
 // var num1 = parseInt(prompt("Enter a number:"));
