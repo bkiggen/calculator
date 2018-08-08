e2c86e6b4f666f577ed5c358ec9718fe115d7b9f
Ben Kiggen and William Wittenbrock <william@>
2018-08-08 11:30:30 -0700

    add css, add click function

--- js/scripts.js
+++ js/scripts.js
@@ -1,57 +1,59 @@
 //Backend Logic
 
 var add = function(number1, number2) {
-  return number1 + number2;
+  return (number1 + number2).toFixed(5);
 };
 var subtract = function(number1, number2) {
-  return number1 - number2;
+  return (number1 - number2).toFixed(5);
 };
 var divide = function(number1, number2) {
-  return number1 / number2;
+  return (number1 / number2).toFixed(5);
 };
 var multiply = function(number1, number2) {
-  return number1 * number2;
+  return (number1 * number2).toFixed(5);
 };
 
 
 //Front End Logic
 
 $(document).ready(function(){
-  $("form#math #add").submit(function(event) {
+  $("form#math").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#math1").val());
-    var number2 = parseInt($("#math2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
+    $("#add").click(function(){
+      var number1 = parseFloat($("#math1").val());
+      var number2 = parseFloat($("#math2").val());
+      var result = add(number1, number2);
+      $("#output").text(result);
+    });
   });
-});
 
-$(document).ready(function(){
-  $("form#math #subtract").submit(function(event) {
+  $("form#math").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#math1").val());
-    var number2 = parseInt($("#math2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
+    $("#subtract").click(function(){
+      var number1 = parseFloat($("#math1").val());
+      var number2 = parseFloat($("#math2").val());
+      var result = subtract(number1, number2);
+      $("#output").text(result);
+    });
   });
-});
 
-$(document).ready(function(){
-  $("form#math #multiply").submit(function(event) {
+  $("form#math").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#math1").val());
-    var number2 = parseInt($("#math2").val());
-    var result = divide(number1, number2);
-    $("#output").text(result);
+    $("#multiply").click(function(){
+      var number1 = parseFloat($("#math1").val());
+      var number2 = parseFloat($("#math2").val());
+      var result = multiply(number1, number2);
+      $("#output").text(result);
+    });
   });
-});
 
-$(document).ready(function(){
-  $("form#math #divide").submit(function(event) {
+  $("form#math").submit(function(event) {
     event.preventDefault();
-    var number1 = parseInt($("#math1").val());
-    var number2 = parseInt($("#math2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
+    $("#divide").click(function(){
+      var number1 = parseFloat($("#math1").val());
+      var number2 = parseFloat($("#math2").val());
+      var result = divide(number1, number2);
+      $("#output").text(result);
+    });
   });
 });
