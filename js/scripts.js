917577420f8651ee6b6cee222fa9406185ff9632
Ben Kiggen and William Wittenbrock <ben@>
2018-08-08 09:16:07 -0700

    update header to include jquery, bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,10 @@
+// $(document).ready(function(){
+//   $("body").click(function(){
+//     alert("hello");
+//   });
+// });
+
+
 var add = function(number1, number2) {
   return parseInt(number1) + parseInt(number2);
 };
@@ -11,7 +18,7 @@ var multiply = function(number1, number2) {
   return parseInt(number1) * parseInt(number2);
 };
 
-var num1 = parseInt(prompt("Enter a number:"));
-var num2 = parseInt(prompt("Enter another number:"));
+// var num1 = parseInt(prompt("Enter a number:"));
+// var num2 = parseInt(prompt("Enter another number:"));
 
-alert(add(num1, num2));
+// alert(add(num1, num2));
