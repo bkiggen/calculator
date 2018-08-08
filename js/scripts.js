9211556f03b8a2b61adafe193343cdcbe3b7a5d8
Ben Kiggen and William Wittenbrock <william@>
2018-08-08 10:30:42 -0700

    Add buttons for add, subtract, multiply, divide, center body and text, add css and javascript functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,4 @@
-// $(document).ready(function(){
-//   $("body").click(function(){
-//     alert("hello");
-//   });
-// });
-
+//Backend Logic
 
 var add = function(number1, number2) {
   return number1 + number2;
@@ -18,7 +13,45 @@ var multiply = function(number1, number2) {
   return number1 * number2;
 };
 
-// var num1 = parseInt(prompt("Enter a number:"));
-// var num2 = parseInt(prompt("Enter another number:"));
 
-// alert(add(num1, num2));
+//Front End Logic
+
+$(document).ready(function(){
+  $("form#math #add").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#math1").val());
+    var number2 = parseInt($("#math2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function(){
+  $("form#math #subtract").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#math1").val());
+    var number2 = parseInt($("#math2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function(){
+  $("form#math #multiply").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#math1").val());
+    var number2 = parseInt($("#math2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
+
+$(document).ready(function(){
+  $("form#math #divide").submit(function(event) {
+    event.preventDefault();
+    var number1 = parseInt($("#math1").val());
+    var number2 = parseInt($("#math2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+});
